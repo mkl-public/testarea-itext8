013ff289a2cdd9bdffc004dd26ecbc14cd73f239
mkl <klink@csi.com>
2023-06-29 12:52:31 +0200

    New SplitInMemory test focusing on the stack overflow question 76544095
    Split a Pdf into byte array pages with IText7
    https://stackoverflow.com/questions/76575599/split-a-pdf-into-byte-array-pages-with-itext7
    
    SplitForMatiasMasso - Tests the improved SplitOnPages method and ByteArrayPdfSplitter class.

---
+++ src/main/csharp/merge/ByteArrayPdfSplitter.cs
@@ -0,0 +1,52 @@
+﻿using iText.Kernel.Pdf;
+using iText.Kernel.Utils;
+using System.Collections.Generic;
+
+namespace iText8.Net_Playground.Merge
+{
+    /// <summary>
+    /// Split a Pdf into byte array pages with IText7
+    /// https://stackoverflow.com/questions/76575599/split-a-pdf-into-byte-array-pages-with-itext7
+    /// 
+    /// This is an improved version of the OP's class.
+    /// </summary>
+    class ByteArrayPdfSplitter : PdfSplitter
+    {
+        private MemoryStream currentOutputStream = null;
+
+        public ByteArrayPdfSplitter(PdfDocument pdfDocument) : base(pdfDocument)
+        {
+        }
+
+        protected override PdfWriter GetNextPdfWriter(PageRange documentPageRange)
+        {
+            currentOutputStream = new MemoryStream();
+            return new PdfWriter(currentOutputStream);
+        }
+
+        public MemoryStream CurrentMemoryStream
+        {
+            get { return currentOutputStream; }
+        }
+
+        public class DocumentReadyListender : IDocumentReadyListener
+        {
+            public List<byte[]> splitPdfs;
+
+            private ByteArrayPdfSplitter splitter;
+
+            public DocumentReadyListender(ByteArrayPdfSplitter splitter, List<byte[]> results)
+            {
+                this.splitter = splitter;
+                this.splitPdfs = results;
+            }
+
+            public void DocumentReady(PdfDocument pdfDocument, PageRange pageRange)
+            {
+                pdfDocument.Close();
+                byte[] contents = splitter.CurrentMemoryStream.ToArray();
+                splitPdfs.Add(contents);
+            }
+        }
+    }
+}
---
+++ src/test/csharp/merge/SplitInMemory.cs
@@ -0,0 +1,51 @@
+﻿using iText.Kernel.Pdf;
+using NUnit.Framework;
+using static iText8.Net_Playground.Merge.ByteArrayPdfSplitter;
+
+namespace iText8.Net_Playground.Merge
+{
+    class SplitInMemory
+    {
+        /// <summary>
+        /// Split a Pdf into byte array pages with IText7
+        /// https://stackoverflow.com/questions/76575599/split-a-pdf-into-byte-array-pages-with-itext7
+        /// 
+        /// This is an improved version of the OP's method.
+        /// </summary>
+        public static List<Byte[]> SplitOnPages(Byte[] bytes)
+        {
+            List <byte[]> result = new List<byte[]>();
+            using (MemoryStream memoryStream = new MemoryStream(bytes))
+            {
+                using (PdfReader reader = new PdfReader(memoryStream))
+                {
+                    PdfDocument docToSplit = new PdfDocument(reader);
+                    ByteArrayPdfSplitter splitter = new ByteArrayPdfSplitter(docToSplit);
+                    splitter.SplitByPageCount(1, new DocumentReadyListender(splitter, result));
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Split a Pdf into byte array pages with IText7
+        /// https://stackoverflow.com/questions/76575599/split-a-pdf-into-byte-array-pages-with-itext7
+        /// 
+        /// Tests the improved <see cref="SplitOnPages"/> method.
+        /// </summary>
+        [Test]
+        public void SplitForMatiasMasso()
+        {
+            Directory.CreateDirectory(@"..\..\..\target\test-outputs\merge");
+
+            byte[] bytes = File.ReadAllBytes(@"..\..\..\src\test\resources\mkl\testarea\itext8\form\form-cms1500.pdf");
+            List<byte[]> resultBytes = SplitOnPages(bytes);
+
+            for (int i = 0; i < resultBytes.Count; i++)
+            {
+                File.WriteAllBytes(@"..\..\..\target\test-outputs\merge\form-cms1500-" + i + ".pdf", resultBytes[i]);
+            }
+        }
+    }
+}
